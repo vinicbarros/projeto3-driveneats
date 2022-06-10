b8bec6e24fc58ecc12f1655930fc91f9782ecb1f
ehtovk <vicaetano_barros@hotmail.com>
2022-06-10 19:23:18 -0300

    Final version javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,9 +9,7 @@ function showGreen(place, selec) {
     validateButton();
 }
 
-
 function validateButton() {
-
     const food = document.querySelector(".food.show");
     const drink = document.querySelector(".drink.show");
     const dessert = document.querySelector(".dessert.show");
@@ -21,7 +19,33 @@ function validateButton() {
     if ((food!=null) && (drink!=null) && (dessert!=null)) {
         check.classList.add('on');
         changeText.innerHTML = "Fechar pedido";
-
+        check.removeAttribute("disabled");
     }
+}
+
+function myValue(price) {
+    return price.innerHTML.replace("R$", "").replace(",", "").trim()/100; 
+}
+
+function order() {
+    const food = document.querySelector(".food.show");
+    const foodValue = myValue(food.querySelector(".price"));
+
+    const drink = document.querySelector(".drink.show");
+    const drinkValue = myValue(drink.querySelector(".price"));
+
+    const dessert = document.querySelector(".dessert.show");
+    const dessertValue = myValue(dessert.querySelector(".price"));
+
+    const total = foodValue + drinkValue  + dessertValue;
+    console.log(total.toFixed(2));
+
+    const message = `Olá, gostaria de fazer o pedido: 
+    - Prato: ${food.querySelector("h3").innerHTML}
+    - Bebida: ${drink.querySelector("h3").innerHTML}
+    - Sobremesa: ${dessert.querySelector("h3").innerHTML}
+    Total: R$ ${total.toFixed(2)}`;
+
+    window.open(`https://wa.me/5512997655354?text=${encodeURIComponent(message)}`);
 }
 
